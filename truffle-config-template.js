df15d91f70a0976477825d6de5921e6be8318fdf
IntegralTeam V <repo@sfxdx.ru>
2020-11-09 14:14:20 +0700

    - add balancer code - change build script

--- truffle-config-template.js
+++ truffle-config-template.js
@@ -79,18 +79,19 @@ module.exports = {
     // timeout: 100000
   },
 
+  contracts_directory: 'contractsDirectory',
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.6.3", // Fetch exact version from solc-bin (default: truffle's version)
+      version: 'solcVersion', // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       settings: { // See the solidity docs for advice about optimization and evmVersion
         optimizer: {
-        enabled: false,
-        runs: 200
+          enabled: false,
+          runs: 200,
+        },
+        evmVersion: 'constantinople',
       },
-      evmVersion: "constantinople"
-      }
     },
   },
 
