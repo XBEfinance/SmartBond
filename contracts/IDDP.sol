8bdf351c35bf2dd180c200d72886d0edfb6b38f1
IntegralTeam Y <repo@sfxdx.ru>
2020-11-13 14:03:40 +0300

    partially implement bond token
    Signed-off-by: IntegralTeam Y <repo@sfxdx.ru>

---
+++ contracts/IDDP.sol
@@ -0,0 +1,6 @@
+pragma solidity >= 0.6.0 < 0.7.0;
+
+
+interface IDDP {
+    function deposit(uint256 tokenId, uint256 value, uint256 maturity) external;
+}
--- contracts/TokenAccessRoles.sol
+++ contracts/TokenAccessRoles.sol
@@ -3,10 +3,11 @@ pragma solidity >= 0.6.0 < 0.7.0;
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
 library TokenAccessRoles {
-    bytes32 private constant MINTER_ROLE = keccak256("NFT_MINTER_ROLE");
-    bytes32 private constant BURNER_ROLE = keccak256("NFT_BURNER_ROLE");
-    bytes32 private constant TRANSFERER_ROLE = keccak256("NFT_TRANSFERER_ROLE");
-    bytes32 private constant ADMINISTRATOR_ROLE = keccak256("NFT_ADMINISTRATOR_ROLE");
+    bytes32 private constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 private constant BURNER_ROLE = keccak256("BURNER_ROLE");
+    bytes32 private constant TRANSFERER_ROLE = keccak256("TRANSFERER_ROLE");
+    bytes32 private constant ADMINISTRATOR_ROLE = keccak256("ADMINISTRATOR_ROLE");
+    bytes32 private constant CONFIGURATOR_ROLE = keccak256("CONFIGURATOR_ROLE");
 
     function minter() public pure returns (bytes32) {
         return MINTER_ROLE;
@@ -22,5 +23,9 @@ library TokenAccessRoles {
 
     function administrator() public pure returns (bytes32) {
         return ADMINISTRATOR_ROLE;
+    }
+
+    function configurator() public pure returns (bytes32) {
+        return CONFIGURATOR_ROLE;
     }
 }
