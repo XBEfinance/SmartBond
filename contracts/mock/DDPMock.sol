222baa623ad57790201bdb8032fb0ebd35740126
IntegralTeam Y <repo@sfxdx.ru>
2020-11-19 01:23:23 +0300

    add ddp mock
    Signed-off-by: IntegralTeam Y <repo@sfxdx.ru>

--- contracts/mock/DDPMock.sol
+++ contracts/mock/DDPMock.sol
@@ -1,7 +1,15 @@
-pragma solidity ^0.4.0;
+pragma solidity >= 0.6.0 < 0.7.0;
 
-contract DDPMock {
-    function DDPMock(){
+import "../IDDP.sol";
+import "../ERC721.sol";
 
+
+contract DDPMock is IDDP, ERC721 {
+    event DepositInvoked(uint256 tokenId, uint256 value, uint256 maturityEnds);
+
+    constructor() ERC721("DDPMock", "DDP") public {}
+
+    function deposit(uint256 tokenId, uint256 value, uint256 maturityEnds) external override {
+        emit DepositInvoked(tokenId, value, maturityEnds);
     }
-}
+} 
