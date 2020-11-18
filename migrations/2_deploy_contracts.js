4d550d24de60b6d989fa504ea36f7e7be65d6831
IntegralTeam Y <repo@sfxdx.ru>
2020-11-18 04:40:13 +0300

    add bond token to deployment
    Signed-off-by: IntegralTeam Y <repo@sfxdx.ru>

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,14 @@
 
 const SecurityAssetToken = artifacts.require('SecurityAssetToken');
 const BondTokenMock = artifacts.require('NFBondTokenMock');
+const BondToken = artifacts.require('BondToken');
 const TokenAccessRoles = artifacts.require('TokenAccessRoles');
 
 module.exports = (deployer, network, accounts) => {
   deployer.then(async () => {
     await deployer.deploy(TokenAccessRoles);
     await deployer.link(TokenAccessRoles, BondTokenMock);
+    await deployer.link(TokenAccessRoles, BondToken);
     await deployer.link(TokenAccessRoles, SecurityAssetToken);
   });
 }
