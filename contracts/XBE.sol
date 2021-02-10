67e97570cd875850ade47db90c2b8205ff3cfcdc
IntegralTeam V <repo@sfxdx.ru>
2021-02-10 16:24:50 +0700

    set XBE as Governments token

--- contracts/XBE.sol
+++ contracts/XBE.sol
@@ -1,16 +1,44 @@
 pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 
 
 /**
- * @title XBE
+ * @title eurxb.finance governments token
  * @dev xbe token
  */
 contract XBE is ERC20 {
+    using Address for address;
+    using SafeMath for uint256;
+
+    address public governance;
+    mapping (address => bool) public minters;
+
     constructor(
         uint256 initialSupply
     ) public ERC20("XBE", "XBE") {
+        governance = _msgSender();
         _mint(msg.sender, initialSupply);
+    }
+
+    function mint(address account, uint256 amount) external {
+        require(minters[msg.sender], "!minter");
+        _mint(account, amount);
+    }
+
+    function setGovernance(address _governance) external {
+        require(msg.sender == governance, "!governance");
+        governance = _governance;
+    }
+
+    function addMinter(address _minter) external {
+        require(msg.sender == governance, "!governance");
+        minters[_minter] = true;
+    }
+
+    function removeMinter(address _minter) external {
+        require(msg.sender == governance, "!governance");
+        minters[_minter] = false;
     }
 }
